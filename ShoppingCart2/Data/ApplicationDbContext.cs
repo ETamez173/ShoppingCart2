e2a67c3c034647242632b89562748e80f418eaf1
ETamez9173 <elitamez17@gmail.com>
2020-08-02 20:47:27 -0500

    first commit

---
+++ ShoppingCart2/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCart2.Models;
+
+namespace ShoppingCart2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+        public DbSet<ShoppingItem> ShoppingItem { get; set; }
+
+    }
+}
---
+++ ShoppingCart2/Models/ShoppingItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart2.Models
+{
+    public class ShoppingItem
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public String ProductName { get; set; }
+
+        [Required]
+        public String ApplicationUserId { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
